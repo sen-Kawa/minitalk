602ece0885717be09c05e915a0e63aca5d588101
olin_tp_ar <myemail@archi.com>
2022-05-29 17:38:40 +0200

    main server function struct for sigaction

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:01:07 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/05/29 16:22:09 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/05/29 17:37:06 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int	main(void)
 {
-//	struct sigaction sa;	//i need a struct for sigaction
-	pid_t	pid;	//pid_t data type specific for process id
+	struct sigaction sa;	//i need a struct for sigaction
+//	pid_t	pid;	//pid_t data type specific for process id
 	
-	pid = getpid();
-	ft_printf("Process ID of server is: %d\n", pid);	
-	ft_printf("test");
-//	sa.
-//	sigaction(SIGUSR1);
+//	pid = getpid();
+	ft_printf("Process ID of server is: %d\n", getpid());	
+	sa.sa_flags = SIGINFO //SIGINFO takes 3 args instead of 2, sa_sigaction has to be set instead of sa_handler
+	sa.sa_sigaction = handler_server;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 
 	return (0);
 }
