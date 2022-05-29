1b1ab512a6455ef04a2c8782b56395f7a09f4306
olin_tp_ar <myemail@archi.com>
2022-05-29 17:41:09 +0200

    test commit

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:01:07 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/05/29 17:37:06 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/05/29 17:41:00 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(void)
 {
 	struct sigaction sa;	//i need a struct for sigaction
 //	pid_t	pid;	//pid_t data type specific for process id
-	
+.	
 //	pid = getpid();
 	ft_printf("Process ID of server is: %d\n", getpid());	
 	sa.sa_flags = SIGINFO //SIGINFO takes 3 args instead of 2, sa_sigaction has to be set instead of sa_handler
